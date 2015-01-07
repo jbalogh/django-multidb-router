3c7fdb2b5ae547f20799f45fa566eb07637f4319
James Socol <me@jamessocol.com>
2015-01-06 19:05:25 -0500

    Real travis matrix

--- setup.py
+++ setup.py
@@ -13,17 +13,21 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    test_requires=['Fabric', 'django-nose'],
+    test_requires=['django-nose'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
-        # I don't know what exactly this means, but why not?
-        'Environment :: Web Environment :: Mozilla',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
