b8f732f1f1eb2b38a55ea60686bc32bc83cd1354
Mathieu Pillard <mpillard@mozilla.com>
2018-09-03 14:13:35 +0200

    Release 0.8

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ copyright = '2015, The Zamboni Collective'
 # built documents.
 #
 # The short X.Y version.
-version = '0.7'
+version = '0.8'
 # The full version, including alpha/beta/rc tags.
-release = '0.7.0'
+release = '0.8.0'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-multidb-router',
-    version='0.7',
+    version='0.8',
     description='Round-robin multidb router for Django.',
     long_description=open('README.rst').read(),
     author='Jeff Balogh',
