a1944a3b5021c321844a861929fa3c5a51e3d590
James Socol <me@jamessocol.com>
2019-01-21 12:50:17 -0500

    Version 0.9

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ copyright = '2019, The Zamboni Collective'
 # built documents.
 #
 # The short X.Y version.
-version = '0.8'
+version = '0.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.8.0'
+release = '0.9.0'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
